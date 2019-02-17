97e349c99a0e0343e9e76b1b0cae6bf4a80fc34e
firehudson <firehudson@gmail.com>
2019-02-17 17:13:16 +0530

    Logout from dashboard if user not present

--- src/modules/dashboard/containers/DashboardContainer.js
+++ src/modules/dashboard/containers/DashboardContainer.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import Sidebar from '../components/Sidebar';
 import { selectUserData } from '../../auth/selectors/user';
+import { logout } from '../../../actions';
 
 const Wrapper = styled('div')`
   width: 100%;
@@ -13,15 +15,32 @@ const Wrapper = styled('div')`
   display: flex;
 `;
 
-const DashboardContainer = ({ user, children, location }) => (
-  <Wrapper>
-    <Sidebar user={user} activePath={location.pathname} />
-    {children}
-  </Wrapper>
-);
+class DashboardContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    if (props.user.id === undefined) { // avoiding the case where id is 0.
+      // if can't find user's information on the dashboard, enforce logout process.
+      props.logout();
+    }
+  }
+
+  render() {
+    const { user, children, location } = this.props;
+
+    return (
+      <Wrapper>
+        <Sidebar user={user} activePath={location.pathname} />
+        {children}
+      </Wrapper>
+    );
+  }
+}
 
 DashboardContainer.propTypes = {
+  logout: PropTypes.func.isRequired,
   user: PropTypes.shape({
+    id: PropTypes.number,
     status: PropTypes.string,
     avatar: PropTypes.string,
   }),
@@ -38,6 +57,10 @@ const mapStateToProps = createStructuredSelector({
   user: selectUserData,
 });
 
-const reduxConnected = connect(mapStateToProps)(DashboardContainer);
+const mapDispatchToProps = dispatch => bindActionCreators({
+  logout,
+}, dispatch);
+
+const reduxConnected = connect(mapStateToProps, mapDispatchToProps)(DashboardContainer);
 
 export default withRouter(reduxConnected);
